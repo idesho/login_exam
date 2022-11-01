cbac33b66adf5a7578fd02062b8846ca8808775f
井出晶 <badminton.1110@i.softbank.jp>
2022-11-01 11:17:05 +0900

    提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy,:edit]
+  resources :users, only: [:new, :create, :show,:edit]
 end
